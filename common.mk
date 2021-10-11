bdf9509dd9cbd805215a9664871dc76f38c2015d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-11 17:51:03 +0200

    sm6250-common: Build vendor interfaces libraries
    Change-Id: I0c8760ff6923c2d2a43ec058554d724ca6600f3c
    
    Signed-off-by: COSMIC <rayavanindra@gmail.com>

--- common.mk
+++ common.mk
@@ -81,6 +81,7 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default
 
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
@@ -156,6 +157,9 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm-service.clearkey
 
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.4.vendor    
+
 # Doze
 PRODUCT_PACKAGES += \
     XiaomiDoze
@@ -189,6 +193,10 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
 
 # GPS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.1.vendor \
+    android.hardware.gnss@2.1.vendor
+
 PRODUCT_PACKAGES += \
     libsensorndkbridge
 
@@ -315,6 +323,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
+
+# Neural networks
+PRODUCT_PACKAGES += \
+    android.hardware.neuralnetworks@1.3.vendor
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
