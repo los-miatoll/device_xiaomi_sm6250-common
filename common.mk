ba180ec4e2a4775d35b77ea8004252f0b3425287
Omkar Chandorkar <gotenksIN@aospa.co>
2022-11-13 19:51:02 +0900

    sm6250-common: overlay: Configure aux camera for Aperture
    Change-Id: I14e9bf9ba71c9d9f4aca04f3b558a1d72e248a60
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Signed-off-by: COSMIC <rayavanindra@gmail.com>

--- common.mk
+++ common.mk
@@ -103,7 +103,8 @@ TARGET_SCREEN_WIDTH := 1080
 # Camera
 PRODUCT_PACKAGES += \
     libgui_vendor \
-    Snap
+    ApertureOverlay \
+    ApertureDevOverlay
 
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
