aa39d81f5af2d4356b9376993e209f7a70333638
COSMIC <rayavanindra@gmail.com>
2023-08-02 02:08:18 +0530

    [SQUASH] Revert "sm6250-common: Build new AIDL power hal"
    This reverts commit b01ba623185be7c42295b8bd7daeb2b884b266a4.
    
    Revert "sm6250-common: rootdir: Override AIDL Power HAL service"
    
    This reverts commit bc319bddf01fb82261fa3062e3b5370ada4010c3.
    
    Revert "sm6250-common: power: Scan input devices for eligible DT2W node"
    
    This reverts commit b55ffa1421cb8aca07844fb141862f102660d09e.
    
    Revert "sm6250-common: power: Bump max interactive duration to 5s"
    
    This reverts commit 221dc4ad29e9e343ab25bd2dd86a35991060b398.
    
    Revert "sm6250-common: power: Add custom mode extension for handling app launch"
    
    This reverts commit 7b21caa7275fb031b4ceddffeb929e68f7d2e662.
    
    Revert "sm6250-common: power: Add custom boost extension for scroll handling"
    
    This reverts commit 42765377e4460ae79aef377ecbf5da69f9a449f8.

--- common.mk
+++ common.mk
@@ -360,7 +360,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power@1.3-service.xiaomi_sm6250
 
 # RCS
 PRODUCT_PACKAGES += \
